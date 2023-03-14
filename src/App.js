9e63dedadc8bc7d39c674cf282bc9c276541fa36
YusraSenimDede <50294199+YusraSenimAydin@users.noreply.github.com>
2023-03-14 17:17:43 +0400

    No match, Nested Route, index Route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
 import { Routes,Route } from "react-router-dom";
 import Home from './components/Home';
 import About from './components/About';
 import Navbar from "./components/Navbar";
+import OrderSummary from "./components/OrderSummary"
+import Products from "./components/Products";
+import FeaturedProducts from "./components/FeaturedProducts";
+import NewProducts from "./components/NewProducts";
+import NoMatch from "./components/NoMatch";
 function App() {
   return (
 
@@ -10,7 +16,13 @@ function App() {
    <Routes>
     <Route path="/" element={<Home/>} />
     <Route path="/about" element={<About/>} />
-
+    <Route path='order-summary' element={<OrderSummary />} />
+    <Route path='products' element={<Products />}>
+       <Route index element={<FeaturedProducts />} />
+          <Route path='featured' element={<FeaturedProducts />} />
+          <Route path='new' element={<NewProducts />} />
+        </Route>
+    <Route path='*' element={<NoMatch />} />
 
    </Routes>
   </>
